b50c773067877d42605e3f168f8cf7d6867242f1
developerMasum <developer.masum2001@gmail.com>
2024-06-29 12:00:30 +0600

    added login credential

--- server/src/app/modules/auth/auth.interface.ts
+++ server/src/app/modules/auth/auth.interface.ts
@@ -1,4 +1,4 @@
-export type TLoginUser = {
-    email: string;
-    password: string;
-  };
+export type TAuth = {
+  email: string;
+  password: string;
+};
--- server/src/app/modules/auth/auth.service.ts
+++ server/src/app/modules/auth/auth.service.ts
@@ -2,27 +2,36 @@ import httpStatus from 'http-status';
 
 import AppError from '../../errors/AppError';
 
-import { TLoginUser } from './auth.interface';
+import { TAuth } from './auth.interface';
+import { Auth } from './auth.model';
 
-const loginUser = async (payload: TLoginUser) => {
-  // checking if the user is exist
-  // const user = await User.isUserExistsByCustomId(payload.email);
-  const user = {
-    password: '12345',
-  };
+const loginUser = async (payload: TAuth) => {
+  console.log(payload);
+  try {
+    // checking if the user exists
+    const user = await Auth.findOne({ email: payload.email });
+    console.log(user);
 
-  if (!user) {
-    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+    }
+
+    // checking if the password matches
+    const passwordMatch = user.password === payload.password;
+
+    if (!passwordMatch) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'User not authorized');
+    }
+
+    return {};
+  } catch (error) {
+    // Handle the error without stopping the server
+    console.error('Error during user login:', error);
+    throw new AppError(
+      httpStatus.INTERNAL_SERVER_ERROR,
+      'An error occurred during login.',
+    );
   }
-  // checking if the user is already deleted
-
-  const passwordMatch = user.password === payload.password;
-
-  if (!passwordMatch) {
-    throw new AppError(httpStatus.UNAUTHORIZED, 'user not authorized');
-  }
-
-  return {};
 };
 
 export const AuthServices = {
